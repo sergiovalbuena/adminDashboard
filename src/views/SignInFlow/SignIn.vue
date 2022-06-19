35cd997cdefe57fb0581295a510e93989b5b6feb
srji <iosergio.valbuena@gmail.com>
2022-06-18 18:16:36 -0700

    dark mode added

--- src/views/SignInFlow/SignIn.vue
+++ src/views/SignInFlow/SignIn.vue
@@ -1,16 +1,37 @@
 <template>
-  <div class="container">
-    <div class="request">
+  <div
+    class="container"
+    :class="{ 'light-background': !isDarkMode, 'dark-background': isDarkMode }"
+  >
+    <div
+      class="request"
+      :class="{ 'light-request': isDarkMode, 'dark-request': !isDarkMode }"
+    >
       Don't have an account?/
       <router-link to="/request"> Request an account</router-link>
     </div>
     <div class="login">
-      <img src="@/assets/logo.png" alt="logo" />
-      <h4>Sign In</h4>
-      <input type="email" placeholder="Email" />
-      <input type="password" placeholder="Password" />
+      <img src="@/assets/logo.png" alt="logo" v-show="isDarkMode" />
+      <img src="@/assets/logo-copy.png" alt="logo" v-show="!isDarkMode" />
+      <h4 :class="{ 'light-text': isDarkMode, 'dark-text': !isDarkMode }">
+        Sign In
+      </h4>
+      <input
+        type="email"
+        placeholder="Email"
+        :class="{ 'light-field': isDarkMode, 'dark-field': !isDarkMode }"
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        :class="{ 'light-field': isDarkMode, 'dark-field': !isDarkMode }"
+      />
       <button>Sign In</button>
-      <router-link to="/recover">Forgot your password?</router-link>
+      <router-link
+        to="/recover"
+        :class="{ 'light-link': isDarkMode, 'dark-link': !isDarkMode }"
+        >Forgot your password?</router-link
+      >
       <button @click="toggleDarkMode">Toggle</button>
     </div>
   </div>
@@ -27,13 +48,54 @@ export default {
   methods: {
     toggleDarkMode() {
       this.isDarkMode = !this.isDarkMode;
-      alert(this.isDarkMode);
+      document.body.style.background = this.isDarkMode ? "#212c4f" : "#c6d0eb";
     },
   },
 };
 </script>
 
 <style scoped lang="scss">
+@import "@/globalStyles/colors.scss";
+@import "@/globalStyles/typography.scss";
+.light-background {
+  background-color: $light-gray;
+}
+.dark-background {
+  background-color: $dark-blue;
+}
+.light-text {
+  color: $white;
+}
+.dark-text {
+  color: $black;
+}
+.light-field {
+  background: rgba(255, 255, 255, 0.2);
+  border: 1px solid rgba(255, 255, 255, 0.2);
+
+  &::placeholder {
+    color: rgba(255, 255, 255, 0.3);
+  }
+}
+.dark-field {
+  background: rgba(198, 208, 235, 0.2);
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  &::placeholder {
+    color: rgba(0, 0, 0, 0.3);
+  }
+}
+.light-request {
+  color: rgba(255, 255, 255, 0.3);
+  a {
+    color: white;
+  }
+}
+.dark-request {
+  color: rgba(0, 0, 0, 0.3);
+  a {
+    color: $black;
+  }
+}
 .container {
   display: flex;
   justify-content: center;
@@ -48,10 +110,6 @@ export default {
   position: absolute;
   top: 40px;
   right: 40px;
-  color: rgba(255, 255, 255, 0.3);
-  a {
-    color: white;
-  }
 }
 h4 {
   margin: 0;
@@ -98,7 +156,12 @@ a {
   font-size: 16px;
   text-align: center;
 
-  color: rgba(255, 255, 255, 0.3);
   text-decoration: none;
+}
+.light-link {
+  color: rgba(255, 255, 255, 0.3);
+}
+.dark-link {
+  color: rgba(0, 0, 0, 0.3);
 }
 </style>
